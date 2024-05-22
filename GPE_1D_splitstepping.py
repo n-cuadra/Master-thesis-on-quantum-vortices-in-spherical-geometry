4ec2aa85ce2b3dcf1201d3ff1bdb2e3fb3c99715
n-cuadra <noel.cuadra@mailbox.org>
2024-05-22 09:06:27 +0200

    Add files via upload

--- GPE_1D_splitstepping.py
+++ GPE_1D_splitstepping.py
@@ -3,20 +3,20 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
 
-#plt.rcParams['animation.ffmpeg_path'] = 'L:/ffmpeg'
+
 
 #simulation parameters
 #############################
 
 
 L = 1.0                              #length of lattice
-N = 256                              #number of lattice points
+N = 256                             #number of lattice points
 x = np.linspace(-L/2, L/2, N, endpoint=False)              #lattice
 dx = L/N                    #lattice spacing   
 dt = 1e-5                             #timestep
 psi = np.zeros(N, dtype=np.complex64)                   #initialized wave function
-sigma = 1./12.
-k = 2 * np.pi * np.fft.fftfreq(n = N, d  = dx)      #lattice in fourier space, positive k first, then negative k
+sigma = 1./12.                             #gaussian width
+k = 2 * np.pi * np.fft.fftfreq(n = N, d  = dx)  / L    #lattice in fourier space, positive k first, then negative k
 g = -1.0                            #nonlinear coefficient
 v = 10. #soliton velocity
 
@@ -28,22 +28,53 @@ def get_norm(psi):
     for i in range(len(psi)):
         norm += dx * abs(psi[i])**2
     return norm
-'''
+
+
 #initalize function as a gaussian
 
-for i in range(N):
-    psi[i] = 1/(np.sqrt(2 * np.pi) * sigma) * np.exp(-(x[i]-L/2)**2 / 2 / sigma**2) 
-'''
+#psi = 1/(np.sqrt(2 * np.pi) * sigma) * np.exp(-x**2 / 2 / sigma**2) 
+
 
 #initalize function as a soliton
 
-psi = np.cosh(3*x) + 3 * np.cosh(x) / (np.cosh(4* x) + 4 * np.cosh(2 * x) + 1)
+psi = 1/np.cosh(10*x)
 
 norm = get_norm(psi)
+
 psi = psi/np.sqrt(norm)
 
 #linear time step 
 
+def linear_timestep(psi, dt):
+    psi = np.fft.fft(psi) #go to fourier space
+    
+    psi = psi * np.exp(-1.0j * k**2 * dt / 2) #in fourier space, do the timestep corresponding to kinetic term of GPE
+    
+    psi = np.fft.ifft(psi) #back to real space
+    
+    psi[-1] = psi[0]  #periodic boundary condition
+    
+    norm = get_norm(psi)
+    
+    psi = psi/np.sqrt(norm) #normalize
+    
+    return psi
+
+
+#nonlinear timestep
+
+def nonlinear_timestep(psi, dt):
+    psi = psi * np.exp(1.0j * g * abs(psi)**2 * dt) #perform timestep for nonlinear term in GPE
+    
+    psi[-1] = psi[0] #periodic boundary conditions
+    
+    norm = get_norm(psi)
+    
+    psi = psi/np.sqrt(norm) #normalize
+    
+    return psi
+
+'''
 def linear_timestep(psi, dt):
     psi = psi[:-1]
     psi_k = np.fft.fft(psi)
@@ -66,7 +97,7 @@ def nonlinear_timestep(psi, dt):
     norm = get_norm(psi_timestep)
     psi_final = np.append(arr = psi_timestep, values = psi_timestep[0])/np.sqrt(norm)
     return psi_final
-
+'''
 
 #split stepping
 
@@ -79,21 +110,23 @@ def split_step(psi, dt):
 #plot function
 
 
+for i in range(10000):
+    psi = split_step(psi, dt)
 
 fig, ax = plt.subplots()
 line, = ax.plot(x, abs(psi)**2)
 #steptext = ax.text(- L/2 + 0.05 , (abs(psi).max())**2 + .5, 'Integration step: 0')
 plt.xlim(-L/2,L/2)
-#plt.ylim((abs(psi).min())**2, (abs(psi).max())**2)
+plt.ylim((abs(psi).min())**2, (abs(psi).max())**2)
 plt.xlabel(r'x')
 plt.ylabel(r'$|\Psi|^2$')
-plt.title('Travelling solitons in the 1D GPE')
+plt.title('Solitons in the 1D GPE')
 #plt.legend()
 
 
 
 #animation
-
+'''
 steps_per_frame = 4
 
 
@@ -112,11 +145,11 @@ def init():
  
 # run animation
  
-ani = animation.FuncAnimation(fig, animate, np.arange(1, 1000), init_func=init, interval=25)
+ani = animation.FuncAnimation(fig, animate, np.arange(1, 1620), init_func=init, interval=25)
 
  
 #FFwriter=animation.FFMpegWriter(fps=60, extra_args=['-vcodec', 'libx264'])
-ani.save('J:/Uni - Physik/Master/6. Semester/Masterarbeit/1D_GPE_split_stepping_soliton.mp4', dpi = 300)
+ani.save('J:/Uni - Physik/Master/6. Semester/Masterarbeit/Media/1D_GPE_split_stepping_gaussian.mp4', dpi = 300)
 
-
+'''
 plt.show()

544f2805c6788aee7682ba3ea6cfc552f4c77fd4
n-cuadra <noel.cuadra@mailbox.org>
2024-05-29 14:02:24 +0200

    Add files via upload

--- spherical GPE solver.py
+++ spherical GPE solver.py
@@ -77,7 +77,53 @@ def get_norm(psi):
     return norm
 
 
+#define derivative wrt azimuthal coordinate
 
+def deriv_phi(psi):
+    sh_coeffs = pysh.expand.SHExpandDHC(griddh = psi, norm = 4, sampling = 2) #expand sh coefficients of psi
+    len_m = np.size(sh_coeffs, axis = 2) 
+    for m in range(len_m):
+        for i in range(2):
+            sh_coeffs[i,:,m] *= 1.0j * m * (-1.)**i  #spherical harmonics are eigenfunctions of del_phi, so need only to multiply coefficients with i*m to perform derivative
+    psi = pysh.expand.MakeGridDHC(sh_coeffs, norm = 4, sampling = 2, extend = False) #back to real space, with the gridded data of the modified coefficients 
+    return psi
+
+#define angular Laplacian
+
+def Laplacian(psi):
+    sh_coeffs = pysh.expand.SHExpandDHC(griddh = psi, norm = 4, sampling = 2) #expand sh coefficients of psi
+    len_l = np.size(sh_coeffs, axis = 1) 
+    for l in range(len_l):
+        sh_coeffs[:,l,:] *= - l * (l + 1) #spherical harmonics are eigenfunctions of angular Laplacian, so need only to multiply coefficients with -l(l+1) to perform Laplacian
+    psi = pysh.expand.MakeGridDHC(sh_coeffs, norm = 4, sampling = 2, extend = False) #back to real space, with the gridded data of the modified coefficients 
+    return psi
+
+#calculate energy of condensate (conserved quantitiy)
+
+def energy(psi):
+    energy = 0
+    Laplace_psi = Laplacian(psi) #array of Laplacian of wavefunction
+    deriv_phi_psi = deriv_phi(psi) #array of derivative wrt azimuthal angle of wavefunction
+    conj_psi = np.conjugate(psi) #array of complex conjugate of wavefunction
+    for i in range(N):
+        for j in range(2*N):
+            energy += dangle**2 * np.sin(theta[i]) * (  - alpha / 2 * conj_psi[i,j] * Laplace_psi[i,j] + g * abs(psi[i,j])**4 - 1.0j * conj_psi[i,j] * deriv_phi_psi[i,j]  ) #compute the hamiltonian
+    energy = np.real(energy)
+    return energy
+    
+#calculate angular momentum of condensate (another conserved quantity).
+
+def ang_momentum(psi):
+    mom = 0
+    deriv_phi_psi = deriv_phi(psi) #array of derivative wrt azimuthal angle of wavefunction
+    conj_psi = np.conjugate(psi) #array of complex conjugate of wavefunction
+    for i in range(N):
+        for j in range(2*N):
+            mom += - 1.0j * dangle**2 * np.sin(theta[i]) * conj_psi[i,j] * deriv_phi_psi[i,j] #compute the angular momentum integral
+    mom = np.real(mom)
+    return mom
+    
+    
 #initialize wavefunction
 
 psi = np.zeros(shape = (N, 2*N), dtype = np.complex128)
@@ -87,21 +133,25 @@ for i in range(N):
         
 norm = get_norm(psi)
 psi  = psi / np.sqrt(norm)
-        
+
+print(get_norm(psi))
+print(energy(psi))    
+print(ang_momentum(psi)) 
 
 sh_coeffs = pysh.expand.SHExpandDHC(griddh = psi, norm = 4, sampling = 2) #calculate array of initial spherical harmonic expansion coefficients 
+
+
 
 #one timestep with split stepping method
 
 def timestep(sh_coeffs, dt):
     len_l = np.size(sh_coeffs, axis = 1)  #size of sh_coeffs array in l and m indices
     for l in range(len_l):
-        sh_coeffs[:,l,:] *= np.exp(- 1.0j * alpha * l * (l + 1) * dt / 2)      #timestep of kinetic term
-    for m in range(len_l):
-        for i in range(2):
-            sh_coeffs[i,:,m] *= np.exp(- 1.0j * m * dt * (-1.)**i)  #timestep of rotating term
+        for m in range(len_l):
+            for i in range(2):
+                sh_coeffs[i,l,m] *= np.exp(- 1.0j * alpha * l * (l + 1) * dt / 2) * np.exp(- 1.0j * m * dt * (-1.)**i)  #timestep of kinetic and rotating term
     grid = pysh.expand.MakeGridDHC(sh_coeffs, norm = 4, sampling = 2, extend = False) #create gridded data in (N, 2*N) array from coeffs
-    grid *= np.exp(1.0j * g * dt * abs(grid)**2) #perform nonlinear timestep
+    grid *= np.exp(1.0j * g * dt * abs(grid)**2) #timestep of nonlinear term
     sh_coeffs = pysh.expand.SHExpandDHC(griddh = grid, norm = 4, sampling = 2) #calculate expansion coefficients from the gridded data
     return sh_coeffs
 
@@ -113,12 +163,17 @@ for q in range(10000):
     sh_coeffs = timestep(sh_coeffs, dt)
     
 
-
+data = pysh.expand.MakeGridDHC(sh_coeffs, norm = 4, sampling = 2, extend = False)
+print(get_norm(data))
+print(energy(data))  
+print(ang_momentum(data))     
+data = abs(data)**2
+coeffs = pysh.expand.SHExpandDH(griddh = data, norm = 4, sampling = 2)
 
 mycmap = cm.seismic
-myproj = crs.Mollweide(central_longitude=180.)
+myprojection = crs.Mollweide(central_longitude=180.)
 
-clm = pysh.SHCoeffs.from_array(sh_coeffs, normalization='ortho', lmax = lmax)
+clm = pysh.SHCoeffs.from_array(coeffs, normalization='ortho', lmax = lmax)
 grid = clm.expand()
 fig, ax = grid.plot(cmap = mycmap, colorbar = 'right',  show = False)
 

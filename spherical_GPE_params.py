4f89bf020a85d67d897741c3db38cbb7fe97a048
n-cuadra <noel.cuadra@mailbox.org>
2024-12-09 09:23:44 +0100

    meshgrid

--- spherical_GPE_params.py
+++ spherical_GPE_params.py
@@ -7,18 +7,17 @@ hbar = 1.055e-22 #hbar in units of kg * µm^2 / s
 
 #simulation parameters (adjustable)
 N = 512 #grid points
-end = 1000 #number of steps in simulation
+end = 100000 #number of steps in simulation
 mass = 1.443e-25 #mass of the atoms in kg
 R = 50.0 #radius of sphere in µm
 scattering_length = 1e-4 #scattering length in units of the radius
-omega_units = -0.207078 #rotating frequency in Hz
-#omega = -2.0 #rotating frequency w/o units
-theta_plus, phi_plus = np.deg2rad(45), np.pi #position of vortex 
-theta_minus, phi_minus = np.pi - theta_plus, np.pi#position of antivortex
+omega_units = 20 #rotating frequency in Hz
+theta_plus, phi_plus = np.deg2rad(45), np.pi   #position of vortex 
+theta_minus, phi_minus = np.pi - theta_plus, np.pi #position of antivortex
 dt = 2e-5  #time step
 bg_dens = 1000 #condensate density far from vortices
 G = 1
-mu = 1660 #chemical potential
+
 
 
 #these are calculated from the values above, don't change these!
@@ -28,13 +27,12 @@ g = - 2 * np.pi / np.log(0.5 * np.sqrt(lmax * (lmax + 1)) * scattering_length * 
 real_dt = 1000 * dt *  R**2 * mass / hbar #one timestep in real time in ms
 omega = omega_units * R**2 * mass / hbar #unitless rotating frequency
 xi = 1/np.sqrt(2 * g * bg_dens) #healing length 
+mu = g * bg_dens + 1
 
 #coordinate system
 
 theta, phi = np.linspace(0,  np.pi, N, endpoint = False), np.linspace(0, 2 * np.pi, 2 * N, endpoint = False)  #rectangular grid with polar angle theta and azimuthal angle phi
 dangle = theta[1] - theta[0]  #grid spacing
-THETA, PHI = np.meshgrid(theta, phi)
+THETA, PHI = np.meshgrid(theta, phi, indexing = 'ij') #the theta and phi values expanded over the whole grid as a N x 2N array
     
-#the theta and phi values expanded over the whole grid as a N x 2N array
-theta_grid = np.tile(theta[:, np.newaxis], reps = (1, 2 * N))
-phi_grid = np.tile(phi[np.newaxis, :], reps = (N, 1))
+
